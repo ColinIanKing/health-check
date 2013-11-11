738a252f10591d122c089b3dd378a554c5121d0f
Colin Ian King <colin.king@canonical.com>
2013-11-11 11:44:53 +0000

    cpustat.c: fix json cpu totals, got user/system/total mixed up
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- cpustat.c
+++ cpustat.c
@@ -194,9 +194,9 @@ int cpustat_dump_diff(json_object *j_tests, const double duration)
 		if ((j_cpu = j_obj_new_obj()) == NULL)
 			goto out;
 		j_obj_obj_add(j_cpustat, "cpu-load-total", j_cpu);
-		j_obj_new_double_add(j_cpu, "cpu-load-total", 100.0 * utime_total);
-		j_obj_new_double_add(j_cpu, "user-cpu-percent", 100.0 * stime_total);
-		j_obj_new_double_add(j_cpu, "system-cpu-percent", 100.0 * ttime_total);
+		j_obj_new_double_add(j_cpu, "user-load-total", 100.0 * utime_total);
+		j_obj_new_double_add(j_cpu, "system-cpu-percent", 100.0 * stime_total);
+		j_obj_new_double_add(j_cpu, "total-cpu-percent", 100.0 * ttime_total);
 	}
 #endif
 	printf("\n");
