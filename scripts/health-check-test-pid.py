ef4d4fe110ddbec6b5f40d1576371ade10e87db0
Colin Ian King <colin.king@canonical.com>
2014-01-03 16:18:17 +0000

    Update copyright dates
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- scripts/health-check-test-pid.py
+++ scripts/health-check-test-pid.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 #
 #
-# Copyright (C) 2013 Canonical
+# Copyright (C) 2013-2014 Canonical
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
