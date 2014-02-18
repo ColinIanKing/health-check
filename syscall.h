0d3c0eee9ab75392b08ca0c3b5722c16048c8a8f
Colin Ian King <colin.king@canonical.com>
2014-02-18 16:33:17 +0000

    Disable syscall handling for x32 mode
    Not yet figured out how to get x32 syscall working, so disable
    it for the moment.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- syscall.h
+++ syscall.h
@@ -24,7 +24,11 @@
 
 #include <sys/time.h>
 
-#if (defined(__x86_64__) || defined(__i386__) || defined(__arm__) || defined(__powerpc__) || defined(__aarch64__))
+#if (( defined(__x86_64__) && defined(__LP64__) ) || \
+     defined(__i386__) || \
+     defined(__arm__) || \
+     defined(__powerpc__) || \
+     defined(__aarch64__))
 #define SYSCALL_SUPPORTED	1
 #else
 #define SYSCALL_SUPPORTED	0
